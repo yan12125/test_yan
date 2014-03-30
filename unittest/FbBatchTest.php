6b150064e32c669e2b9a58b644dafc85f0be15b8
Yen Chi Hsuan <yan12125@gmail.com>
2014-03-31 01:10:35 +0800

    Add a test case for FbBatch

--- unittest/FbBatchTest.php
+++ unittest/FbBatchTest.php
@@ -8,5 +8,15 @@ class FbBatchTest extends PHPUnit_Framework_TestCase
         $result = Fb::api('/100002601797125');
         $this->assertEquals($result['name'], "Yen ChiHsuan");
     }
+
+    public function testMultipleGet()
+    {
+        $req = new FbBatch();
+        $req->push(null, '/100002601797125');
+        $req->push(null, '/100002601797125');
+        $result = $req->run();
+        $this->assertEquals($result[0]['name'], "Yen ChiHsuan");
+        $this->assertEquals($result[1]['name'], "Yen ChiHsuan");
+    }
 }
 ?>
