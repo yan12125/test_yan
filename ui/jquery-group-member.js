4e66aa921b10e17579116b80bebf22ef7e1e1ea7
Yen Chi Hsuan <yan12125@gmail.com>
2014-05-18 05:05:38 +0800

    Group member chooser functionality done

--- ui/jquery-group-member.js
+++ ui/jquery-group-member.js
@@ -2,7 +2,36 @@ $.widget("custom.facebookGroupMemberCompleter", $.ui.autocomplete, {
     options: {
         source: function (request, response) {
             searchGroupAutoComplete(request.term, response, this.options.token, this.options.gid);
+        }, 
+        select: function (event, data) {
+            var $parent = $(this).parent();
+            var input = $parent.find('input');
+            var displayField = $parent.find('.display-field');
+            var btnDelete = $('<button>').text('Remove').button().click(function (event) {
+                input.val('');
+                displayField.hide();
+                input.show();
+            });
+            displayField
+                .empty()
+                .append(
+                    $('<a>')
+                        .attr('href', 'https://www.facebook.com/' + data.item.uid)
+                        .text(data.item.name)
+                )
+                .append(btnDelete)
+                .show();
+            input.hide();
         }
+    }, 
+    _create: function () {
+        this._superApply(arguments);
+
+        this._wrapper = $('<span>').addClass('facebookGroupMemberCompleter-wrapper');
+        this._displayField = $('<span>');
+        this._wrapper.insertAfter(this.element);
+        this.element.detach().appendTo(this._wrapper);
+        this._wrapper.append($("<span>").addClass('display-field'));
     }, 
     _renderItem: function (ul, item) {
         var li = $('<li>').addClass('facebook-group-member');
