ca840db197895d949348d6ed0008eb541d0d40ed
Yen Chi Hsuan <yan12125@gmail.com>
2015-04-19 00:44:33 +0800

    Fix YahhoImgSearch
    The domain name changes...an additional 's'

--- plugins/yahooImgSearch.php
+++ plugins/yahooImgSearch.php
@@ -1,38 +1,37 @@
 <?php
 class YahooImgSearch extends PluginBase
 {
-    protected $ch = null;
-
-    public function __construct()
-    {
-        $this->ch = curl_init();
-    }
-
-    public function __destruct()
-    {
-        curl_close($this->ch);
-    }
+    private $content = null;
 
     public function run($param)
     {
-        curl_setopt_array($this->ch, array(
-            CURLOPT_URL => 'http://tw.image.search.yahoo.com/search/images?fr=yfp&ei=UTF-8&p='.$param, 
-            CURLOPT_RETURNTRANSFER => 1, 
+        $this->url = 'http://tw.images.search.yahoo.com/search/images?fr=yfp&ei=UTF-8&p='.urlencode($param);
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $this->url,
+            CURLOPT_RETURNTRANSFER => 1,
             CURLOPT_USERAGENT => Util::FIREFOX_UA
         ));
-        $data = curl_exec($this->ch);
+        $this->content = curl_exec($ch);
+        curl_close($ch);
+
         $dom = new simple_html_dom();
-        $dom->load($data);
-        $items = $dom->find('.ld');
+        $dom->load($this->content);
+        $items = $dom->find('.ld a');
         $n = rand(0, count($items) - 1);
-        $url1 = $items[$n]->children(0)->href;
-        $data2 = null;
+        $url1 = $items[$n]->href;
+        $dom->clear();
         parse_str(parse_url($url1, PHP_URL_QUERY), $data2);
         return $param."\n".'http://'.$data2['imgurl'];
     }
 
     public function handleException($e)
     {
+        return array(
+            'message' => $e->getMessage(),
+            'url' => $this->url,
+            'content' => $this->content,
+        );
     }
 }
 ?>
